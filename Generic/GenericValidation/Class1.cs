cca7ee19e73f21ae2a3f1044765afdb25a71898a
unknown <vilfrack21@gmail.com>
2019-11-17 22:43:33 -0300

    Se crea clase generic

---
+++ Generic/GenericValidation/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericValidation
+{
+    public class GenericValidation<T>
+    {
+        private T genericMemberVariable;
+
+        public T Validation(T value)
+        {
+            var o = value.GetType();
+            switch (o.Name)
+            {
+                case "DateTime":
+                    Console.WriteLine(o.Name);
+                    break;
+                case "Int32":
+                    Console.WriteLine(o.Name);
+                    break;
+                default:
+                    Console.WriteLine(o.Name);
+                    break;
+            }
+            //genericMemberVariable = value;
+            return genericMemberVariable;
+        }
+    }
+}
